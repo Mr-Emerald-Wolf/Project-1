854f05543606c83a9d780e21acd6f063372ca558
Mr-Emerald-Wolf <50650788+Mr-Emerald-Wolf@users.noreply.github.com>
2019-05-16 00:02:45 +0530

    Add files via upload

---
+++ School_Scripts_May19/Basic_Script_Circle_10-5-19.py
@@ -0,0 +1,9 @@
+#Area and Perimeter of Circle
+
+r = int(input("Enter Radius of Circle: "))
+p = 22/7
+c = 2*p*r
+a  =  p*(r**2)
+
+print("The Circumference of Circle is ",c,"\n","The Area of Circle is ",a)     
+
