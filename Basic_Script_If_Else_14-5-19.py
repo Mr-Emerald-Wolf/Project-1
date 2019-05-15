14a45f2f9619fd6cf95dd16b48c6bc5f9c2217ca
Mr-Emerald-Wolf <50650788+Mr-Emerald-Wolf@users.noreply.github.com>
2019-05-15 22:05:00 +0530

    Add files via upload

---
+++ Basic_Script_If_Else_14-5-19.py
@@ -0,0 +1,45 @@
+#Printing from 0-9
+while 1==1:
+     try:
+          a = int(input("Enter Number from 0 to 9: "))
+          if a == 0:
+                    print("Zero")
+                    break
+          elif a == 1:
+                    print("One")
+                    break
+          elif a == 2:
+                    print("Two")
+                    break
+          elif a == 3:
+                    print("Three")
+                    break
+          elif a == 4:
+                    print("Four")
+                    break
+          elif a == 5:
+                    print("Five")
+                    break
+          elif a == 6:
+                    print("Six")
+                    break
+          elif a == 7:
+                    print("Seven")
+                    break
+          elif a == 8:
+                    print("Eight")
+                    break
+          elif a == 9:
+                    print("Nine")
+                    break
+          elif a == 10:
+                    print("Exit")
+                    break
+          else:
+                    print("Enter a Number Between 0-9 ","\n","Enter 10 To Exit")
+     except:
+          print("Dawg Find Those Errors")
+
+print("Battle Symphony")
+
+          
