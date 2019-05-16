b72c439adc2ea31e293ab2c72396c1e6db7858b0
Mr-Emerald-Wolf <50650788+Mr-Emerald-Wolf@users.noreply.github.com>
2019-05-16 08:30:10 +0530

    Add files via upload

---
+++ School_Scripts_May19/UTKARSH JHA.py
@@ -0,0 +1,31 @@
+print("'                                                       HERE IS YOUR REPORT ANALYSIS                                                   '")
+t=700
+at=100
+e=float(input("ENTER YOUR MARKS IN-ENGLISH"))
+h=float(input("ENTER YOUR MARKS IN-HINDI"))
+m=float(input("ENTER YOUR MARKS IN-MATHS"))
+s=float(input("ENTER YOUR MARKS IN-SCIENCE"))
+sst=float(input("ENTER YOUR MARKS IN-SOCIAL SCIENCE"))
+cs=float(input("ENTER YOUR MARKS IN-COMPUTER SCIENCE"))
+sy=float(input("ENTER YOUR MARKS IN-PSYCHOLOGY"))
+
+a=((e+h+m+s+cs+sst+sy)/7)
+p=((e+h+m+s+cs+sst+sy)/700)(100)
+
+
+sp= input("enter the subject whose percentage you want to know")
+if sp=='ENGLISH':
+    ep=((e/100)(100))
+    if sp=='MATHS':
+        ep=((e/100)(100))
+        if sp=='SCIENCE':
+            ep=((e/100)(100))
+            if sp=='SOCIAL SCIENCE':
+                ep=((e/100)(100))
+                if sp=='COMPUTER SCIENCE':
+                    ep=((e/100)(100))
+                    if sp=='HINDI':
+                        ep=((e/100)(100))
+                        if sp=='PSYCOLOGY':
+                            ep=((e/100)(100))
+                            
