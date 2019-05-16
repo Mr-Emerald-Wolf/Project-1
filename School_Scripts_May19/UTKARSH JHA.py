677f20e0f5f224d862c683c312607f631953a498
Mr-Emerald-Wolf <50650788+Mr-Emerald-Wolf@users.noreply.github.com>
2019-05-16 08:39:22 +0530

    Add files via upload

--- School_Scripts_May19/UTKARSH JHA.py
+++ School_Scripts_May19/UTKARSH JHA.py
@@ -11,21 +11,28 @@ sy=float(input("ENTER YOUR MARKS IN-PSYCHOLOGY"))
 
 a=((e+h+m+s+cs+sst+sy)/7)
 p=((e+h+m+s+cs+sst+sy)/700)(100)
-
+print("YOUR AVERAGE IS--", a)
+print("YOUR TOTAL PERCENTAGE IS--", p)
 
 sp= input("enter the subject whose percentage you want to know")
 if sp=='ENGLISH':
     ep=((e/100)(100))
-    if sp=='MATHS':
-        ep=((e/100)(100))
-        if sp=='SCIENCE':
-            ep=((e/100)(100))
-            if sp=='SOCIAL SCIENCE':
-                ep=((e/100)(100))
-                if sp=='COMPUTER SCIENCE':
-                    ep=((e/100)(100))
-                    if sp=='HINDI':
-                        ep=((e/100)(100))
-                        if sp=='PSYCOLOGY':
-                            ep=((e/100)(100))
-                            
+    print(ep)
+elif sp=='MATHS':
+    ep=((m/100)(100))
+    print(ep)
+elif sp=='SCIENCE':
+    ep=((s/100)(100))
+    print(ep)
+elif sp=='SOCIAL SCIENCE':
+    ep=((sst/100)(100))
+    print(ep)
+elif sp=='COMPUTER SCIENCE':
+    ep=((cs/100)(100))
+    print(ep)
+elif sp=='HINDI':
+    ep=((h/100)(100))
+    print(ep)
+elif sp=='PSYCOLOGY':
+    ep=((sy/100)(100))
+    print(ep)                        
