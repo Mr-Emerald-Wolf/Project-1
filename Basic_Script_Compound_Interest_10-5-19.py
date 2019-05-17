ea6503aa82356fb65de2b6537f1d2cc749f5c369
Mr-Emerald-Wolf <callofdutyuss@gmail.com>
2019-05-17 22:35:12 +0530

    Revert "Merge pull request #1 from Mr-Emerald-Wolf/Mr-Emerald-Wolf-patch-1"
    This reverts commit 73abe9fce7896df3b009d8814bf697cd1f073c93, reversing
    changes made to 026d0f51fc629812adde1a805516a9873652d639.

--- Basic_Script_Compound_Interest_10-5-19.py
+++
@@ -1,6 +0,0 @@
-#Making Compound Interest
-p = int(input("Enter Principle: "))
-r = int(input("Enter Rate of Interest: "))
-t = int(input("Enter Time Period in Years: "))
-c  = p*((1+r)/100)**t
-print("The compund interest is ",c," and the total amount to be paid after",t,"years is",p+c)
--- Tuples, Lists, Dictionaries_8-5-19.py
+++
@@ -1,6 +0,0 @@
-a = [1, 2, 3, 4]
-
-i = int(input("Enter Item "))
-a[1:3]  
-print(a[1:3])
-
