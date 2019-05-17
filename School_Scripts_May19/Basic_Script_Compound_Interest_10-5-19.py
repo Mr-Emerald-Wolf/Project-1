aa298f455e5850a3391b86dda6107499c0e80a64
Mr-Emerald-Wolf <50650788+Mr-Emerald-Wolf@users.noreply.github.com>
2019-05-17 22:37:20 +0530

    Update Basic_Script_Compound_Interest_10-5-19.py

--- School_Scripts_May19/Basic_Script_Compound_Interest_10-5-19.py
+++ School_Scripts_May19/Basic_Script_Compound_Interest_10-5-19.py
@@ -3,4 +3,4 @@ p = int(input("Enter Principle: "))
 r = int(input("Enter Rate of Interest: "))
 t = int(input("Enter Time Period in Years: "))
 c  = p*((1+r)/100)**t
-print("The compund interest is ",c," and the total amount to be paid after",t,"years is",p+c)
+print("The compound interest is ",c," and the total amount to be paid after",t,"years is",p+c)
